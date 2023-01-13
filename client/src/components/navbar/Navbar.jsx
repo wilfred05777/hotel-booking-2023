7204054b22da1ab7b6a237471efb5b35d2c5db91
Wilfred <wilfred05777@gmail.com>
2023-01-13 15:38:34 +0800

    navbar style

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <div className="navContainer">
         <span className="logo">Julian's Hotel</span>
         <div className="navItems">
-          <button className="navbutton">Register</button>
+          <button className="navButton">Register</button>
           <button className="navButton">Login</button>
         </div>
       </div>
