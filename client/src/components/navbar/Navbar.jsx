6e37d2cae9ca6543698dce11943a323949ee08d4
Wilfred <wilfred05777@gmail.com>
2023-01-13 13:44:44 +0800

    header and navbar components initial

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import './navbar.scss'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className="navContainer">
+        <span className="logo">Julian's Hotel</span>
+        <div className="navItems">
+          <button className="navbutton">Register</button>
+          <button className="navButton">Login</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
