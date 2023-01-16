b1935facdc32a927afcac531ad1b60e7d3d65c8e
Wilfred <wilfred05777@gmail.com>
2023-01-16 11:56:14 +0800

    1:01:30 Footer Component Start

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="fLists"></div>
+    </div>
+  );
+};
+
+export default Footer;
